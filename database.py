82406771cc7aed5486a9f04d62d7dd22c7d58f1a
ajaysinghrajput9370 <ajaysinghrajput9370@gmail.com>
2025-08-14 08:01:18 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,6 @@
 import sqlite3
-import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_NAME = os.path.join(BASE_DIR, "users.db")
+DB_NAME = "users.db"
 
 def init_db():
     conn = sqlite3.connect(DB_NAME)
