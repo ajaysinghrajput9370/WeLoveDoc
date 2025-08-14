bc8ae30559f34799019bf5b4c7e1c644aa472ff4
ajaysinghrajput9370 <ajaysinghrajput9370@gmail.com>
2025-08-14 07:47:53 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,10 @@
 import sqlite3
 import os
 
-# Full path ensure kare taaki server/local dono me sahi kaam kare
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_NAME = os.path.join(BASE_DIR, "users.db")
 
 def init_db():
-    """Create database and users table if not exists."""
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
     c.execute("""
@@ -22,5 +20,4 @@ def init_db():
     conn.close()
 
 def get_db_connection():
-    """Return a new database connection."""
     return sqlite3.connect(DB_NAME)
