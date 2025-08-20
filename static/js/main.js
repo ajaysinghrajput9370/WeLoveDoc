564ccd706f1d085e62ad6bb1ffd8d27302175121
ajaysinghrajput9370 <ajaysinghrajput9370@gmail.com>
2025-08-20 11:08:51 +0530

    Delete static/js/main.js

--- static/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('#signup-btn').click(function(){ $('#subscription-popup').show(); });
-  $('#cancel-btn').click(function(){ $('#subscription-popup').hide(); });
-  $('#subscribe-btn').click(function(){
-    alert('Subscription successful!');
-    $('#subscription-popup').hide();
-  });
-});
